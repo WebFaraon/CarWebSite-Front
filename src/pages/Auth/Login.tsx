5acdc10c3bb3f7e35c120cc717c85d4441b16c11
AndrreyG <andreigandrabur6@gmail.com>
2026-02-24 23:55:26 +0200

    Changed login page html structure

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -3,13 +3,15 @@ import "../../components/navbar/Navbar"
 
 function Login() {
     return(
-        <div className="login-container">
-            <div className="login-inner-container">
-                <h1 className="login-title">LOG IN TO YOUR ACCOUNT</h1>
-                <input className="email-input" type="email" placeholder="Email"></input>
-                <input className="password-input" type="password" placeholder="Password"></input>
-                <button className="Login">LOGIN</button>
-                <p className="forgot-password">Forgot password?</p>
+        <div className="wrapper">
+            <div className="card">
+                <h2 className="title">Log In to your account</h2>
+                <form className="form">
+                    <input className="input" type="email" placeholder="Email"></input>
+                    <input className="input" type="password" placeholder="Password"></input>
+                    <button className="submit-button" type="submit"></button>
+                    <a className="forgot-password-link" href="#"></a>
+                </form>
             </div>
         </div>
     );
