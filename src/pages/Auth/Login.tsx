9ad11b3dbfa14f60c9c8025075d6b50260561d1f
AndrreyG <andreigandrabur6@gmail.com>
2026-02-24 23:47:30 +0200

    x

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -5,6 +5,11 @@ function Login() {
     return(
         <div className="login-container">
             <div className="login-inner-container">
+                <h1 className="login-title">LOG IN TO YOUR ACCOUNT</h1>
+                <input className="email-input" type="email" placeholder="Email"></input>
+                <input className="password-input" type="password" placeholder="Password"></input>
+                <button className="Login">LOGIN</button>
+                <p className="forgot-password">Forgot password?</p>
             </div>
         </div>
     );
