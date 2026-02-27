865933ce3f5352300cbaa844494949e6ff5ca9c2
AndrreyG <andreigandrabur6@gmail.com>
2026-02-27 14:29:37 +0200

    fixed things

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,12 @@ import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home/Home.tsx'
 import SellCar from './pages/SellCar/SellCar.tsx'
 import Login from './pages/Auth/Login.tsx'
-<<<<<<< HEAD
 import Favorites from './pages/Favorites/Favorites.tsx'
 import CatalogPage from './pages/Catalog/CatalogPage.tsx'
 import Signup from './pages/Auth/Signup.tsx'
 import ContactUs from './pages/ContactUs/ContactUs.tsx'
 import CarDetails from './pages/CarDetails/CarDetails.tsx'
 import ForgotPassword from './pages/Auth/ForgotPassword.tsx'
-=======
-import CatalogPage from './pages/Catalog/CatalogPage.tsx'
-import Signup from './pages/Auth/Signup.tsx'
-import ContactUs from './pages/ContactUs/ContactUs.tsx'
-import Favorites from './pages/Favorites/Favorites.tsx'
->>>>>>> victor
 
 function AppRoutes() {
   const location = useLocation()
@@ -28,12 +21,9 @@ function AppRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/contact-us" element={<ContactUs />} />
-<<<<<<< HEAD
         <Route path="/car-details" element={<CarDetails />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-=======
         <Route path="/offers" element={<CatalogPage />} />
->>>>>>> victor
 
       </Routes>
     </div>
