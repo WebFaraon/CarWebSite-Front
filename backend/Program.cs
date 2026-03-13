055b749b3f6fc11b54f335d20b3eff321d9c2adc
AndrreyG <andreigandrabur6@gmail.com>
2026-03-13 10:55:33 +0200

    Added swagger api

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowFrontend", policy => {
         policy.WithOrigins("http://localhost:5173")
